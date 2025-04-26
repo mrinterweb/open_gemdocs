4dc5d7674412c9ad8788bcebfc18538609a663cf
Sean McCleary <sean.mccleary@reserv.com>
2025-04-25 23:44:01 -0700

    Basic functionality to open gemdocs.org

--- lib/open_gemdocs.rb
+++ lib/open_gemdocs.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "open_gemdocs/version"
+require_relative 'open_gemdocs/version'
+require_relative 'open_gemdocs/browser'
 
 module OpenGemdocs
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/open_gemdocs/browser.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module OpenGemdocs
+  class Browser
+    attr_reader :gem_name, :version, :use_latest
+
+    def initialize(gem_name:, version: nil, use_latest: false)
+      @gem_name = gem_name
+      raise ArgumentError, 'Gem name is required' if gem_name.nil? || gem_name.empty?
+
+      @version = version
+      @use_latest = use_latest
+    end
+
+    def resolve_version
+      return version if version
+
+      if !use_latest && File.exist?('Gemfile.lock')
+        @version = check_bundle_version
+        if @version
+          puts "Using version from Gemfile.lock: #{version}"
+          return version
+        end
+      end
+
+      @use_latest = true
+
+      puts 'No version specified, using latest version'
+    end
+
+    def open_browser
+      resolve_version
+      raise Error, 'No version URL found' unless version_url
+
+      version_str = version ? "@v#{version}" : ''
+      latest_str = use_latest ? ' (latest)' : ''
+      puts "Fetching gem documentation for #{gem_name}#{version_str}#{latest_str}..."
+      # open is a macOS command to open a URL in the default browser
+      raise Error, 'Could not resolve URL' if version_url.nil?
+
+      `open "#{version_url.sub('production.', '')}"`
+    end
+
+    def version_url
+      if use_latest
+        version_data.last['url']
+      else
+        version_data.detect { |row| row['version'] == version }&.fetch('url')
+      end
+    end
+
+    def check_bundle_version
+      `bundle show #{gem_name}`.strip.match(/#{gem_name}-([0-9.]+).*$/)&.[](1)
+    end
+
+    def version_data
+      @version_data ||= begin
+        uri = URI("https://gemdocs.org/gems/#{ARGV[0]}/versions.json")
+        res = Net::HTTP.get_response(uri)
+        unless res.is_a?(Net::HTTPSuccess)
+          raise Error, "HTTP request failed to uri: #{uri} -- #{res.code} #{res.message}"
+        end
+
+        JSON.parse(res.body)['versions']
+      end
+    end
+  end
+end
