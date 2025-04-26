0e2c9e1ab13aa2b4a07d15dc2d4d79d09e899e71
Sean McCleary <sean.mccleary@reserv.com>
2025-04-25 23:23:12 -0700

    Initial commit

---
+++ spec/open_gemdocs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OpenGemdocs do
+  it "has a version number" do
+    expect(OpenGemdocs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
