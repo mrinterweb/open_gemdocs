e1e384eda9e7ca6319ecebfcf7a4a3062160f367
Sean McCleary <sean.mccleary@reserv.com>
2025-04-26 02:04:25 -0700

    Adding local docs with yard option

--- lib/open_gemdocs.rb
+++ lib/open_gemdocs.rb
@@ -2,6 +2,7 @@
 
 require_relative 'open_gemdocs/version'
 require_relative 'open_gemdocs/browser'
+require_relative 'open_gemdocs/yard'
 
 module OpenGemdocs
   class Error < StandardError; end
---
+++ lib/open_gemdocs/yard.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module OpenGemdocs
+  module Yard
+    extend self
+    SERVER_COMMAND = 'yard server --gems --daemon ./'
+
+    def browse_gem(gem_name)
+      if server_running?
+        puts "Yard server is already running. Opening browser..."
+        system("open http://localhost:8808/docs/#{gem_name}")
+      else
+        puts "Starting Yard server in the background..."
+        start_yard_server
+        sleep 2 # Give the server some time to start
+        system("open http://localhost:8808/docs/#{gem_name}")
+      end
+      puts "  When you're done, remember to stop the server with `open-gem-docs --stop`"
+    end
+
+    def yard_installed?
+      `gem list yard -i`.strip == 'true'
+    end
+
+    def start_yard_server = `#{SERVER_COMMAND}`
+
+    def server_running? = find_yard_pids.any?
+
+    def find_yard_pids
+      # Find pids bound to port 8808
+      `lsof -i TCP:8808 | grep -E "^ruby.*:8808"`.strip.split("\n").map { |line| line.split(/\s+/)[1] }
+    end
+
+    def stop_server
+      yard_pids = find_yard_pids
+      if yard_pids.any?
+        puts "Stopping Yard server processes: #{yard_pids.join(', ')}"
+        `kill #{yard_pids.join(' ')}`
+        puts "Yard server processes stopped."
+      else
+        puts "No Yard server processes found to stop"
+      end
+    end
+  end
+end
